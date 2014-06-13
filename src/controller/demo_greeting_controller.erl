90f8ace5099e7c737aee2ef637a80504549c826c
Lance Morgan <lance@lancemorgan.com>
2014-06-13 13:00:53 -0600

    first commit

---
+++ src/controller/demo_greeting_controller.erl
@@ -0,0 +1,51 @@
+-module(demo_greeting_controller, [Req]).
+-compile(export_all).
+
+% Raw HTML Action
+hello('GET', []) ->
+    {output, "Hello!"}.
+
+% JSON Action
+%hello('GET', []) ->
+%   {json, [{greeting, "Hello, world!"}]}.
+
+% Template Action
+%hello('GET', []) ->
+%    {ok, [{greeting, "Hello, world!"}]}.
+
+% List Action
+%list('GET', []) ->
+%    Greetings = boss_db:find(greeting, []),
+%    {ok, [{greetings, Greetings}]}.
+
+% Create Action
+%create('GET', []) ->
+%    ok;
+%create('POST', []) ->
+%    GreetingText = Req:post_param("greeting_text"),
+%    NewGreeting = greeting:new(id, GreetingText),
+%    {ok, SavedGreeting} = NewGreeting:save(),
+%    {redirect, [{action, "list"}]}.
+
+% Goodbye Controller
+%goodbye('POST', []) ->
+%    boss_db:delete(Req:post_param("greeting_id")),
+%    {redirect, [{action, "list"}]}.
+
+% Test Message Action
+%send_test_message('GET', []) ->
+%    TestMessage = "Free at last!",
+%    boss_mq:push("test-channel", TestMessage),
+%    {output, TestMessage}.
+
+% Pull Greetings Action
+%pull('GET', [LastTimestamp]) ->
+%    {ok, Timestamp, Greetings} = boss_mq:pull("new-greetings",
+%        list_to_integer(LastTimestamp)),
+%    {json, [{timestamp, Timestamp}, {greetings, Greetings}]}.
+
+% Live Action
+%live('GET', []) ->
+%    Greetings = boss_db:find(greeting, []),
+%    Timestamp = boss_mq:now("new-greetings"),
+%    {ok, [{greetings, Greetings}, {timestamp, Timestamp}]}.
